115e285581e62f8955ce6e6d72b93c1ef475be9a
luaresp <luaresp@OmenLuaresp>
2020-09-09 22:24:22 -0300

    base project

---
+++ programingExercise/src/test/java/cl/luaresp/ProgramingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.luaresp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

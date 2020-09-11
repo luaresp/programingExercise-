fa46be34e5c6b37be99ef0f4cdffc28e25f9fc1b
luaresp <luaresp@OmenLuaresp>
2020-09-10 23:43:44 -0300

    exception specific

---
+++ programingExercise/src/main/java/cl/luaresp/exception/BadRequestException.java
@@ -0,0 +1,23 @@
+package cl.luaresp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+	private static final long serialVersionUID = 3147110056440873748L;
+
+	public BadRequestException(String message) {
+		super(message);
+	}
+
+	public BadRequestException(Throwable cause) {
+		super(cause);
+	}
+
+	public BadRequestException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
